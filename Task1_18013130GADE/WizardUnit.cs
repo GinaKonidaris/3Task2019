a223708c0f0bb95691fc34e4f3673dff3e87cff7
GinaKonidaris <ginakonidaris@gmail.com>
2019-10-07 00:42:21 +0200

    Added WizardUnit

---
+++ Task1_18013130GADE/WizardUnit.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_18013130GADE
+{
+    class WizardUnit : Unit
+    {
+        public int Attack { get; internal set; }
+        public int Team { get; internal set; }
+        public int Xpos { get; internal set; }
+        public int Ypos { get; internal set; }
+        public int Health { get; internal set; }
+        public string Symbol { get; internal set; }
+        public override void Move(Direction d)
+        { //moves unit in another direction
+            switch (d)
+            {
+                case Direction.North:
+                    {
+                        ypos -= speed;
+                        break;
+                    }
+                case Direction.East:
+                    {
+                        xpos += speed;
+                        break;
+                    }
+                case Direction.West:
+                    {
+                        xpos -= speed;
+                        break;
+                    }
+                case Direction.South:
+                    {
+                        ypos += speed;
+                        break;
+                    }
+            }
+        }
+        public WizardUnit(int x, int y, int Speed, int Range, int Health, int Team, string Symbol, int Attack)
+        {
+            xpos -= x;
+            ypos = y;
+            Health = this.Health;
+            Speed = speed;
+            Range = range;
+            Team = team;
+            Symbol = this.Symbol;
+            Attack = attack;
+        }
+
+        private int DistanceTo(Unit u)
+        { //checks the distance between units
+            if (u.GetType() == typeof(MeleeUnit))
+            {
+                MeleeUnit n = (MeleeUnit)u;
+                int d = (xpos - n.Xpos) + Math.Abs(ypos - n.Ypos);
+                return d;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public Direction DirectionTo(Unit u)
+        {    //changes direction
+            if (u.GetType() == typeof(MeleeUnit))
+            {
+                MeleeUnit n = (MeleeUnit)u;
+                if (n.Xpos < n.Ypos)
+                {
+                    return Direction.North;
+                }
+                else if (n.Xpos > xpos)
+                {
+                    return Direction.South;
+                }
+                else if (n.Ypos < ypos)
+                {
+                    return Direction.West;
+                }
+                else
+                {
+                    return Direction.East;
+                }
+            }
+            else
+            {
+                return Direction.North;
+            }
+        }
+        public override void Combat(Unit u)
+        {  //starts combat
+            if (u.GetType() == typeof(MeleeUnit))
+            {
+                Health -= ((MeleeUnit)u).Attack;
+
+            }
+            else if (u.GetType() == typeof(RangedUnit))
+            {
+                Health -= ((RangedUnit)u).Attack;
+                Health -= ((RangedUnit)u).Attack;
+            }
+        }
+        public override bool Inranged(Unit u)
+        { //checks if unit is in range
+            if (u.GetType() == typeof(MeleeUnit))
+            {
+                MeleeUnit n = (MeleeUnit)u;
+                if (DistanceTo(u) <= range)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+        public override Unit Closest(Unit[] units)
+        {   //checks for the closest unit to
+            Unit closest = this;
+            int closestDistance = 50;
+
+            foreach (Unit u in units)
+            {
+                if (((MeleeUnit)u).Team == team)
+                {
+                    if (DistanceTo(u) < closestDistance)
+                    {
+                        closest = u;
+                        closestDistance = DistanceTo((MeleeUnit)u);
+                    }
+                }
+            }
+            return closest;
+        }
+        public override bool Isdead()
+        {  //checks if unit is alive
+            if (Health < +0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public override string Tostring()
+        {   //gives information on MeleeUnit
+            return "MU" + xpos + "," + ypos + "," + Health + "," + Name + team;
+        }
+    }
+}
